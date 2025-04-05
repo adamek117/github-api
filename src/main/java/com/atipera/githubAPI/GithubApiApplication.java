7215619d6866f332d37a264a509ced1b3e74abba
adamek117 <adamp200011@gmail.com>
2025-04-05 13:19:33 +0200

    added resttemplate

--- src/main/java/com/atipera/githubAPI/GithubApiApplication.java
+++ src/main/java/com/atipera/githubAPI/GithubApiApplication.java
@@ -2,6 +2,8 @@ package com.atipera.githubAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GithubApiApplication {
@@ -9,5 +11,10 @@ public class GithubApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubApiApplication.class, args);
 	}
+
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
